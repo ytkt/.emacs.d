4355244c3e8ddf300f7bd18908d64583f9782704
ytkt <yyttkktt@gmail.com>
2019-05-22 02:45:59 +0900

    Setup about appearance

---
+++ conf/02-appearance.el
@@ -0,0 +1,15 @@
+(menu-bar-mode 0)
+(setq inhibit-startup-message t)
+
+;; buffer
+(column-number-mode nil)
+(size-indication-mode t)
+(size-indication-mode t)
+
+;; line number
+(global-linum-mode t)
+(setq linum-format "%4d ")
+
+;; truncate lines
+(setq-default truncate-partial-width-windows t)
+(setq-default truncate-lines t)
