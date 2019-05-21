cab35e179a684b06593b6be192f007149d51ddbf
ytkt <yyttkktt@gmail.com>
2019-05-22 02:34:43 +0900

    Setup General configration (language, ring, audo-save)

---
+++ conf/01-general.el
@@ -0,0 +1,13 @@
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; snooze beep
+(setq ring-bell-function 'ignore)
+
+;; Auto save files
+(setq auto-save-timeout 15)
+(setq auto-save-interval 60)
+(add-to-list 'backup-directory-alist
+                         (cons "." "~/.emacs.d/backup/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backup/") t)))
