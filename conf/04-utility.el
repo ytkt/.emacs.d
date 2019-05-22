4a30fd5803fbe7c5741507ccb4a66d8d9a64e0a7
ytkt <yyttkktt@gmail.com>
2019-05-23 06:20:17 +0900

    Setup wgrep-mode

--- conf/04-utility.el
+++ conf/04-utility.el
@@ -18,3 +18,9 @@
 (ido-everywhere 1)
 (setq ido-enable-flex-matching t)
 (setq ido-save-directory-list-file "~/emacs.d/tmp/ido.last")
+
+
+;; wgrep-mode
+(setf wgrep-enable-key "e")
+(setq wgrep-auto-save-buffer t)      ; wgrep終了時にバッファを保存
+(setq wgrep-change-readonly-file t)  ; read-only bufferにも変更を適用する
