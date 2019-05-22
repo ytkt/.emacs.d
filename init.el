238a82c98f152a27b503e95de36a817333c6c32b
ytkt <yyttkktt@gmail.com>
2019-05-23 02:28:04 +0900

    Change package manager from Cask to el-get

--- init.el
+++ init.el
@@ -1,16 +1,25 @@
-;;; cask
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
 
-(require 'cask)
-(cask-initialize)
+(el-get-bundle init-loader)
+
+;; Appearance
+(el-get-bundle color-theme-sanityinc-tomorrow)
+
+;; Utilities
+(el-get-bundle yasnippet)
+(el-get-bundle auto-complete)
+
 
 ;;; init-loader.el
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+
